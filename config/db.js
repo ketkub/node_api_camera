59c0fa77cc2a0467e5189a55144df5a5747d570a
ketkub <ketwowwow@gmail.com>
2024-10-15 20:50:52 +0700

    de

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('apinodedb_aoqa', 'nodeapi', 'dSmqzXdcsrbFIZ1RqpGrE81mw6mVJvhz', {
-host: 'dpg-cs4evdd2ng1s739jbgt0-a',
+const sequelize = new Sequelize('nodeapi_db', 'postgres', '123456', {
+host: 'localhost',
 dialect: 'postgres',
 });
 
